d1a2e6305f2616928b4e819d1a8d21e7eed89cb5
Kalin1603 <kalintodorov03@gmail.com>
2022-02-20 12:35:55 +0200

    Added Exam

---
+++ ИП_модул 12/Задача Практически казус-20220220/ProjectManagement-Problems/ProjectManagement-Problems/ProjectManagement/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Entities;
+using ProjectManagement.Repositories;
+using ProjectManagement.ViewModels.Home;
+using ProjectManagement.ExtentionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjectManagement.ActionFilters;
+
+namespace ProjectManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginVM model)
+        {
+            if (!this.ModelState.IsValid)
+                return View(model);
+
+            ProjectManagementDbContext context = new ProjectManagementDbContext();
+            User loggedUser = context.Users.Where(u => u.Username == model.Username &&
+                                                       u.Password == model.Password)
+                                           .FirstOrDefault();
+
+            HttpContext.Session.SetObject("loggedUser", loggedUser);
+
+            if (loggedUser == null)
+            {
+                this.ModelState.AddModelError("autheError", "Invalid username or password!");
+                return View(model);
+            }
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AuthenticationFilter]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("loggedUser");
+
+            return RedirectToAction("Login", "Home");
+        }
+    }
+}
---
+++ ИП_модул 12/Задача Практически казус-20220220/ProjectManagement-Problems/ProjectManagement-Problems/ProjectManagement/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Entities
+{
+    public class User
+    {
+        public User()
+        {
+            this.Projects = new HashSet<Project>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual ICollection<Project> Projects { get; set; }
+    }
+}
